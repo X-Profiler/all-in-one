383a88c91f5dfbbbf8e2bf0b6e7e8b3580e8f518
hyj1991 <yeekwanvong@gmail.com>
2022-11-29 12:24:09 +0800

    feat: custom auth

--- app.js
+++ app.js
@@ -6,20 +6,29 @@ class AppBoot {
   }
 
   didReady() {
-    const { options: { serverScope }, config: { ignoreRouter }, router } = this.app;
+    const { options: { serverScope }, config: { ignoreRouter, customAuth }, router } = this.app;
 
     // handle console router
     if (serverScope === 'console') {
-      const layers = router.stack;
-      for (const layer of layers) {
-        if (ignoreRouter.every(router => typeof router === 'string'
-          ? router !== layer.path
-          : router.path !== layer.path || !layer.methods.includes(router.method.toUpperCase()))) {
-          continue;
-        }
 
-        this.app.logger.info(`[devtoolx-console] ignore router: %s, methods: %s`, layer.path, layer.methods.join(','));
-        layer.stack.pop();
+      // custom auth
+      if (customAuth) {
+        this.app.middleware.unshift(customAuth);
+      }
+
+      // ignore routes
+      if (ignoreRouter.length) {
+        const layers = router.stack;
+        for (const layer of layers) {
+          if (ignoreRouter.every(router => typeof router === 'string'
+            ? router !== layer.path
+            : router.path !== layer.path || !layer.methods.includes(router.method.toUpperCase()))) {
+            continue;
+          }
+
+          this.app.logger.info(`[devtoolx-console] ignore router: %s, methods: %s`, layer.path, layer.methods.join(','));
+          layer.stack.pop();
+        }
       }
     }
   }
