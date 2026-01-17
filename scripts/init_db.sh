ab86368d0e8f6cd48a70902a50deb5327aa646a9
MK <fengmk2@gmail.com>
2026-01-17 21:05:32 +0800

    fix: add --skip-ssl for MySQL client in Docker environment

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -14,11 +14,14 @@ DB_USERNAME=${DB_USERNAME:-root}
 # current directory
 CWD=$(cd $(dirname $0); pwd)
 
-mysql -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -e 'CREATE DATABASE IF NOT EXISTS `xprofiler_console`;'
-mysql -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_console' < ${CWD}/init_xprofiler_console.sql
-mysql -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_console' -e 'SHOW tables;'
+# MySQL connection options (--skip-ssl for self-signed certs in Docker)
+MYSQL_OPTS="--skip-ssl"
 
-mysql -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -e 'CREATE DATABASE IF NOT EXISTS `xprofiler_logs`;'
-mysql -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_logs' < ${CWD}/init_xprofiler_logs.sql
-mysql -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_logs' < ${CWD}/init_xprofiler_logs_date.sql
-mysql -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_logs' -e 'SHOW tables;'
+mysql ${MYSQL_OPTS} -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -e 'CREATE DATABASE IF NOT EXISTS `xprofiler_console`;'
+mysql ${MYSQL_OPTS} -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_console' < ${CWD}/init_xprofiler_console.sql
+mysql ${MYSQL_OPTS} -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_console' -e 'SHOW tables;'
+
+mysql ${MYSQL_OPTS} -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -e 'CREATE DATABASE IF NOT EXISTS `xprofiler_logs`;'
+mysql ${MYSQL_OPTS} -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_logs' < ${CWD}/init_xprofiler_logs.sql
+mysql ${MYSQL_OPTS} -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_logs' < ${CWD}/init_xprofiler_logs_date.sql
+mysql ${MYSQL_OPTS} -u${DB_USERNAME} ${PASSWORD} -h ${DB_SERVER_HOST} -P 3306 -D 'xprofiler_logs' -e 'SHOW tables;'
